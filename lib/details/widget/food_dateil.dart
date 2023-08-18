f17ecba8a2cfc9d0743e7be8884b4c9a4f1f8410
zharkynaishabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-08-18 14:41:16 +0600

    finished zharkynay app

--- lib/details/widget/food_dateil.dart
+++ lib/details/widget/food_dateil.dart
@@ -10,88 +10,111 @@ class FoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(25),
+      padding: const EdgeInsets.all(25),
       color: kBackgraund,
       child: Column(
         children: [
           Text(
-            food.name, style: const TextStyle(
-            fontSize: 24, 
-            fontWeight: FontWeight.bold,
-            ),),
-            const SizedBox(width: 15,),
-            Padding(
-              padding:  const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildIconText(Icons.access_time_outlined,
-                  Colors.blue,
-                  food.waitTime),
-                  _buildIconText(Icons.star_outline,
-                  Colors.amber,
-                  food.score.toString()),
-                  _buildIconText(Icons.local_fire_department_outlined,
-                 Colors.red,
-                  food.cal),
-                ],
-              ),
+            food.name,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
-             const SizedBox(height: 20,),
-            FoodQuantity(food: food,),
-            const SizedBox(height: 30,),
-            Row(
-              children: const[
-                Text("Ingredients", style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18
-                ),)
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildIconText(
+                    Icons.access_time_outlined, Colors.blue, food.waitTime),
+                _buildIconText(
+                    Icons.star_outline, Colors.amber, food.score.toString()),
+                _buildIconText(
+                    Icons.local_fire_department_outlined, Colors.red, food.cal),
               ],
             ),
-             const SizedBox(height: 10,),
-            Container(
-              height: 100,
-              child: ListView.separated(
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          FoodQuantity(
+            food: food,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Row(
+            children: [
+              Text(
+                "Ingredients",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 100,
+            child: ListView.separated(
                 scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) =>  Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Column(
-                    children: [
-                      Image.asset(food.ingridients[index].values.first,width: 52,),
-                      Text(food.ingridients[index].keys.first),
-                    ],
-                  ),
-                ),
-                separatorBuilder: (_, index) => const SizedBox(width: 20,), 
+                itemBuilder: (context, index) => Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            food.ingridients[index].values.first,
+                            width: 52,
+                          ),
+                          Text(food.ingridients[index].keys.first),
+                        ],
+                      ),
+                    ),
+                separatorBuilder: (_, index) => const SizedBox(
+                      width: 20,
+                    ),
                 itemCount: food.ingridients.length),
-            ),
-            const SizedBox(height: 30,),
-            Row(
-              children: const[
-                Text("About", style: 
-                TextStyle(
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Row(
+            children: [
+              Text(
+                "About",
+                style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 18,
-                ),)
-              ],
-            ),
-            const SizedBox(height: 25,),
-            Text(food.about, style: const TextStyle(
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Text(
+            food.about,
+            style: const TextStyle(
               fontSize: 18,
               height: 1.5,
               wordSpacing: 1.2,
-            ),),
-            const Padding(padding: EdgeInsets.all(40))
-          ],  
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(40))
+        ],
       ),
     );
   }
 
-  Widget _buildIconText(IconData icon, Color color,String text) {
+  Widget _buildIconText(IconData icon, Color color, String text) {
     return Row(
       children: [
         Icon(
@@ -99,9 +122,10 @@ class FoodDetail extends StatelessWidget {
           color: color,
           size: 20,
         ),
-        Text(text,
-        style: 
-        const TextStyle(fontSize: 16),)
+        Text(
+          text,
+          style: const TextStyle(fontSize: 16),
+        )
       ],
     );
   }
--- lib/screens/home/widget/restauran_info.dart
+++ lib/screens/home/widget/restauran_info.dart
@@ -1,5 +1,3 @@
-// import 'package:flutter/src/widgets/framework.dart';
-// import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app_dl/constants/colors.dart';
 import 'package:food_app_dl/models/restaurant.dart';
@@ -10,85 +8,98 @@ class RestaurantInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      margin:  const EdgeInsets.only(top: 40,),
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
       padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(restaurant.name,
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  restaurant.name,
                   style: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 10,),
-                  Row(
-                    children: [
-                      Container(
+                      fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Container(
                         padding: const EdgeInsets.all(5),
                         decoration: BoxDecoration(
                           color: Colors.grey.withOpacity(0.4),
                           borderRadius: BorderRadius.circular(5),
                         ),
-                        child: Text(restaurant.waitTime,
-                        style: 
-                         const TextStyle(color: Colors.white),)
-                         ),
-                    const SizedBox(width: 10,),
-                      Text(restaurant.distanse,
-                      style: 
-                       TextStyle(
-                       fontSize: 16,
-                       fontWeight: FontWeight.bold,
-                       color: Colors.grey.withOpacity(0.4)),
-                      ),
-                    const SizedBox(width: 10,),
-                      Text(restaurant.label,
-                      style: 
-                       TextStyle(fontSize: 16,
-                       fontWeight: FontWeight.bold,
-                       color: Colors.grey.withOpacity(0.4)))
-                    ],
-                  )
-                ],
+                        child: Text(
+                          restaurant.waitTime,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      restaurant.distanse,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey.withOpacity(0.4)),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(restaurant.label,
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey.withOpacity(0.4)))
+                  ],
+                )
+              ],
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                'assets/res_logo.png',
+                width: 60,
               ),
-              
-              ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                child: Image.asset('assets/res_logo.png',width:60,),
-              )
-            ],
-          ),
-          const SizedBox(height: 5,),
-          Row(
-           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-           children: [
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
             Text(
-            restaurant.decs,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              restaurant.decs,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
             ),
             Row(
               children: [
-               const Icon(Icons.star_outline,
-                color: kPrimaryColor,),
-               Text('${restaurant.score}',
-               style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-               ),
-              ),
-             const  SizedBox(height: 15,)
+                const Icon(
+                  Icons.star_outline,
+                  color: kPrimaryColor,
+                ),
+                Text(
+                  '${restaurant.score}',
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 15,
+                )
               ],
             )
-           ],
-          )
-        ]
-        ),
+          ],
+        )
+      ]),
     );
   }
 }
